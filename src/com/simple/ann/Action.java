97d1045fa10311abfd85a88db995ba93d6bbd8bd
chenxi <cxchen84xi@hotmail.com>
2015-11-03 09:25:46 +0800

    Test

--- src/com/simple/ann/Action.java
+++ src/com/simple/ann/Action.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 //用来标记是否是控制类
+//false
 @Retention(RetentionPolicy.RUNTIME)  
 @Target(ElementType.METHOD)  
 public @interface Action {
